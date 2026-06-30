436e15ad5f67dec1d69b64e3281f6c51660af61b
JordanIsr <jordanjordaniza@gmail.com>
2026-06-29 22:40:52 -0500

    module matriculas

--- src/app/pages/matriculas/matriculas.ts
+++ src/app/pages/matriculas/matriculas.ts
@@ -2,10 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-matriculas',
-  imports: [],
+  standalone: true,
   templateUrl: './matriculas.html',
-  styleUrl: './matriculas.scss',
+  styleUrls: ['./matriculas.scss']
 })
 export class Matriculas {
+
+  registrar() {
+    console.log('Registrar matrícula');
+  }
+
+  actualizar() {
+    console.log('Actualizar matrícula');
+  }
+
+  anular() {
+    console.log('Anular matrícula');
+  }
+
+  buscar() {
+    console.log('Buscar matrícula');
+  }
 
 }
